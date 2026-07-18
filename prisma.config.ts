c6c104b4999d65c9119bb4c01e650b468620a1e4
kelvynkhrystian <khrystian.dev@hotmail.com>
2026-07-18 16:29:14 -0300

    fix(prisma): configure datasource using prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,14 @@
+import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
+
   migrations: {
     path: 'prisma/migrations',
   },
+
   datasource: {
-    url: '${DIRECT_URL}',
+    url: process.env.DATABASE_URL!,
   },
 });

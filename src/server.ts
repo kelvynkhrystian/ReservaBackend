ea6b8c4357da7328255ba1741e281b0e03f476f7
kelvynkhrystian <khrystian.dev@hotmail.com>
2026-07-18 22:59:06 -0300

    test2

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,28 @@
-import 'dotenv/config';
-import { app } from './app.js';
+// import 'dotenv/config';
+// import { app } from './app.js';
 
-const port = Number(process.env.PORT) || 3333;
+// const port = Number(process.env.PORT) || 3333;
+
+// app.listen(port, '0.0.0.0', () => {
+//   console.log('INICIANDO...');
+//   console.log(`Servidor Express rodando na porta ${port}`);
+//   console.log('JWT:', !!process.env.JWT_SECRET);
+//   console.log('DATABASE:', !!process.env.DATABASE_URL);
+//   console.log('SERVIDOR ONLINE');
+// });
+
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_, res) => {
+  res.send('OK');
+});
+
+const port = Number(process.env.PORT) || 3000;
+
+console.log('ANTES DO LISTEN');
 
 app.listen(port, '0.0.0.0', () => {
-  console.log('INICIANDO...');
-  console.log(`Servidor Express rodando na porta ${port}`);
-  console.log('JWT:', !!process.env.JWT_SECRET);
-  console.log('DATABASE:', !!process.env.DATABASE_URL);
-  console.log('SERVIDOR ONLINE');
+  console.log('SERVIDOR SUBIU!');
 });
